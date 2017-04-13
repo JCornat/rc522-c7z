96b6c5deeaf7ccb445b937526fa67ebb87bb43cd
Jacques Cornat <cornat.jacques@gmail.com>
2017-04-13 20:04:03 +0200

    change src

--- index.ts
+++ index.ts
@@ -13,13 +13,13 @@ let initChildProcess = () => {
   child = spawn('node', [__dirname + '/' + 'rc522_output.js']);
   let linereader = readline.createInterface(child.stdout, child.stdin);
 
-  linereader.on('line', function (rfidTagSerialNumber) {
+  linereader.on('line', (rfidTagSerialNumber) => {
     if (registeredCallback instanceof Function) {
       registeredCallback(rfidTagSerialNumber);
     }
   });
 
-  child.on('close', function (code) {
+  child.on('close', () => {
     if (!mainProcessShutdown) {
       initChildProcess();
     }
@@ -41,13 +41,7 @@ process.once('exit', () => {
   child.kill();
 });
 
-// This is a somewhat ugly approach, but it has the advantage of working
-// in conjunction with most of what third parties might choose to do with
-// uncaughtException listeners, while preserving whatever the exception is.
 process.once('uncaughtException', (error) => {
-  // If this was the last of the listeners, then shut down the child and rethrow.
-  // Our assumption here is that any other code listening for an uncaught
-  // exception is going to do the sensible thing and call process.exit().
   if (process.listeners('uncaughtException').length === 0) {
     mainProcessShutdown = true;
     child.kill();
